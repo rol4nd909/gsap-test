d27a03517f76e0c9fb3b1ace6ba051fd848030e1
Rol4nd <r.franke@eagerly.nl>
2021-12-17 11:56:47 +0100

    V2.0.0

--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,24 @@
 import "../scss/style.scss";
 
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+import gsap from "gsap";
 import { preloadImages, calcDrawImage } from "./utils";
+import ScrollTrigger from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
 // Sequence trigger animation
 (() => {
-  // generate array of images paths. length = frames length, see: /src/assets/img
-  const urls = [...new Array(147)].map((value, index) => `/${index + 1}.jpg`);
+  // get all images
+  const urls = [...new Array(147)].map((value, index) => `img-sequence/${index + 1}.jpg`);
 
   // load images async
   const images = preloadImages(urls);
 
   const container = document.querySelector(".cb-sequence");
-  const canvas = container.querySelector(".airpods");
+  const canvas = container.querySelector("canvas");
   const ctx = canvas.getContext("2d");
 
   // create "scrub" ScrollTrigger effect with pin of main block
-  // pinType: transform is required when use pin with smooth scrollbar
   const tl = new gsap.timeline({
     scrollTrigger: {
       trigger: container,
@@ -27,7 +26,6 @@ gsap.registerPlugin(ScrollTrigger);
       start: "top top",
       end: "200%", // scene duration
       pin: true,
-      pinType: "transform",
     },
   });
 
@@ -61,50 +59,3 @@ gsap.registerPlugin(ScrollTrigger);
     window.addEventListener("resize", () => calcDrawImage(ctx, imgs[counter.i]));
   });
 })();
-
-// (() => {
-//   gsap.to(".box", {
-//     scrollTrigger: {
-//       trigger: ".gsap-trigger",
-//       start: "top top",
-//       end: "200%",
-//       scrub: true,
-//       pin: true,
-//       pinType: "transform",
-//       //   markers: true,
-//     },
-//     // xPercent: "100",
-//     x: "30rem",
-//   });
-// })();
-
-// (() => {
-//   gsap.from(".gsap-stagger", {
-//     scrollTrigger: {
-//       trigger: ".gsap-stagger",
-//     },
-//     y: 20,
-//     opacity: 0,
-//     stagger: 0.2,
-//   });
-// })();
-
-// let tl = gsap.timeline({
-//   // yes, we can add it to an entire timeline!
-//   scrollTrigger: {
-//     trigger: ".trigger",
-//     snap: {
-//       snapTo: "labels", // snap to the closest label in the timeline
-//       duration: { min: 0.2, max: 3 }, // the snap animation should be at least 0.2 seconds, but no more than 3 seconds (determined by velocity)
-//       delay: 0.2, // wait 0.2 seconds from the last scroll event before doing the snapping
-//       ease: "power1.inOut",
-//     },
-//   },
-// });
-
-// // add animations and labels to the timeline
-// tl.addLabel("start").from(".gsap-stagger", {
-//   y: 20,
-//   opacity: 0,
-//   stagger: 0.2,
-// });
